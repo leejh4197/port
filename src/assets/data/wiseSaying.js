be9518e2cb1e5fe9af12bf0092788bcd1af39833
leejh4197 <leejh4197@naver.com>
2024-01-10 14:50:19 +0900

    [FEAT]title작업

---
+++ src/assets/data/wiseSaying.js
@@ -0,0 +1,11 @@
+export const wiseSaying = [
+  { first: "seize", second: "the day" },
+  { first: "you only", second: "live once" },
+  { first: "follow", second: "your heart" },
+  { first: "past is", second: "just past" },
+  { first: "don't beat", second: "yourself up" },
+  { first: "life is", second: "a journey" },
+  { first: "don't dream,", second: "be it" },
+  { first: "no pain,", second: "no gain" },
+  { first: "no sweet,", second: "no sweet" },
+];
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { wiseSaying } from "../assets/data/wiseSaying";
 
 const Intro = () => {
-  return <div>Intro</div>;
+  const [random, setRandom] = useState({});
+
+  useEffect(() => {
+    const randomIDX = Math.floor(Math.random() * wiseSaying.length);
+    setRandom(wiseSaying[randomIDX]);
+  }, []);
+  return (
+    <section id="intro">
+      <div class="intro__inner">
+        <h1 class="intro__title">frontend developer</h1>
+        <div class="intro__lines" aria-hidden="true">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+        <div class="intro__text">
+          <div class="text">
+            {random && (
+              <>
+                <div>{random.first}</div>
+                <div>{random.second}</div>
+              </>
+            )}
+          </div>
+          <div class="img">
+            <img src="/portImg.jpg" alt="어바웃" />
+          </div>
+        </div>
+        <div class="intro__lines bottom" aria-hidden="true">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
